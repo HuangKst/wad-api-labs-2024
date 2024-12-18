749ed3df625b0d3498153ad1691175019c9a5f40
HuangKst <1300105758@qq.com>
2024-12-18 18:38:14 +0000

    added routing for posting a new task

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -8,8 +8,13 @@ const app = express();
 
 const port = process.env.PORT;
 
+// 添加 JSON 解析中间件
+app.use(express.json());
+
+// 添加任务路由
 app.use('/api/tasks', tasksRouter);
 
+// 启动服务器
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
